c896e36ba7d33daaf6f2fb8af3f79d290110b16f
unknown <prodvictorkaio@gmail.com>
2023-07-07 22:59:21 -0300

    error-hubOK

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,8 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import axios from "axios";
-
 export default function SignUpPage() {
   const navigate = useNavigate();
   const [nome, setNome] = useState("");
@@ -10,29 +9,6 @@ export default function SignUpPage() {
   const [senha, setSenha] = useState("");
   const [confirmarSenha, setConfirmarSenha] = useState("");
   const [error, setError] = useState("");
-
-  useEffect(() => {
-    // Verificar se o usuário já está autenticado ao carregar a página
-    const token = localStorage.getItem("token");
-    if (token) {
-      const decodedToken = decodeToken(token);
-      if (decodedToken) {
-        setNome(decodedToken.email); // Define o nome do usuário logado
-        navigate("/home");
-      } else {
-        navigate("/"); // Redireciona para a página de login se o token for inválido ou expirado
-      }
-    }
-  }, [navigate]);
-
-  const decodeToken = (token) => {
-    try {
-      const decoded = jwt.decode(token);
-      return decoded;
-    } catch (error) {
-      return null;
-    }
-  };
 
   const handleNomeChange = (event) => {
     setNome(event.target.value);
@@ -59,26 +35,15 @@ export default function SignUpPage() {
     }
 
     try {
-      const response = await axios.post(
-        `${import.meta.env.VITE_API_URL}/usuario/cadastro`,
-        {
-          nome,
-          email,
-          senha,
-          confirmarSenha,
-        }
-      );
+      await axios.post(`${import.meta.env.VITE_API_URL}/usuario/cadastro`, {
+        nome,
+        email,
+        senha,
+        confirmarSenha,
+      });
 
-      const token = response.data.token; // Obter o token retornado pela API
-
-      // Armazenar o token no LocalStorage
-      localStorage.setItem("token", token);
-
-      // Configurar o cabeçalho com o token
-      setAuthHeader(token);
-
-      // Redirecionar o usuário para a página de home
-      navigate("/home");
+      // Redirecionar o usuário para a página de login
+      navigate("/");
     } catch (error) {
       console.error("Erro ao cadastrar usuário:", error.response.data.error);
       setError(
@@ -86,15 +51,6 @@ export default function SignUpPage() {
       );
     }
   };
-
-  const setAuthHeader = (token) => {
-    if (token) {
-      axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-    } else {
-      delete axios.defaults.headers.common["Authorization"];
-    }
-  };
-
   return (
     <SignUpContainer>
       <form onSubmit={handleCadastroSubmit}>
